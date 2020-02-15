c4dad4b5ee6920c1f6b0ac67d2897bb14f65dd32
lycheeshell <lycheeshell@163.com>
2020-02-15 21:53:34 +0800

    play detail

--- pages/game/detail/detail.js
+++ pages/game/detail/detail.js
@@ -8,7 +8,8 @@ Page({
   data: {
     playId: '',
     studentId: '',
-    students: []
+    students: [],
+    tipContent: ''
   },
 
   onLoad: function(options) {
@@ -16,9 +17,79 @@ Page({
       playId: options.playId,
       studentId: options.studentId
     })
-    
+
+    //查找组局的所有参与者
+    wxRequest({
+      url: 'team/play/member',
+      content_type: 'application/x-www-form-urlencoded; charset=UTF-8',
+      data: {
+        'playId': this.data.playId
+      },
+      success: (res) => {
+        console.log(res);
+        this.setData({
+          students: res.data.data
+        })
+      },
+    });
   },
 
+  joinPlay(e) {
+    wxRequest({
+      url: 'team/play/addParticipant',
+      content_type: 'application/x-www-form-urlencoded; charset=UTF-8',
+      method: 'POST',
+      data: {
+        'playId': this.data.playId,
+        'studentId': this.data.studentId
+      },
+      success: (res) => {
+        console.log(res);
+        if(res.data.status == 200) {
+          this.showTip('加入组局成功！');
+        }
+      },
+    });
+  },
+
+  quitPlay(e) {
+    wxRequest({
+      url: 'team/play/leaveParticipant',
+      content_type: 'application/x-www-form-urlencoded; charset=UTF-8',
+      method: 'POST',
+      data: {
+        'playId': this.data.playId,
+        'studentId': this.data.studentId
+      },
+      success: (res) => {
+        console.log(res);
+        if (res.data.status == 200) {
+          this.showTip('退出组局成功！');
+        }
+      },
+    });
+  },
+
+  showInfo(e) {
+    var studentId = e.currentTarget.id;
+    // itr 2 展示对方的信息
+
+
+
+  },
+
+  showTip(msg) {
+    this.setData({
+      tip: 'tip',
+      tipContent: msg
+    })
+  },
+
+  hideTip(e) {
+    this.setData({
+      tip: null
+    })
+  },
 
   showModal(e) {
     this.setData({
